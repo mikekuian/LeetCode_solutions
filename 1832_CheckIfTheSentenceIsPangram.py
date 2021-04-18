146342d64793a5f54075a07ab28a6e4d9eaa6db0
mikekuian <michaelkuian@gmail.com>
2021-04-17 23:27:51 -0500

    new problem

---
+++ 1832_CheckIfTheSentenceIsPangram.py
@@ -0,0 +1,18 @@
+# A pangram is a sentence where every letter of the English alphabet appears at least once.
+
+# Given a string sentence containing only lowercase English letters, return true if sentence is a pangram, or false otherwise.
+
+class Solution:
+    def checkIfPangram(self, sentence: str) -> bool:
+
+        import collections
+        from collections import Counter
+
+        alphabet = "abcdefghijklmnopqrstuvwxyz"
+        l_alphabet = [ord(alphabet[i]) - ord('a') + 1 for i in range(26)]
+
+        for char in sentence:
+            if l_alphabet[ord(char) - ord('a')]:
+                l_alphabet[ord(char) - ord('a')] = 0
+
+        return not sum(l_alphabet)
